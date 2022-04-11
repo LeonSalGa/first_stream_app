f2d552bdda2f36ed0ccf01bdc8eaab8ec8838435
LeonSalGa <leonardosalcedog@gmail.com>
2022-04-11 13:54:57 -0500

    Update streamlit_app.py
    typo index

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Let's put a pick list here so they can pick the fruit the wnat to include
-streamlit.multiselect("pick some fruits: ",list(my_fruit_list.idex))
+streamlit.multiselect("pick some fruits: ", list(my_fruit_list.index))
 
 #display the table on the page
 streamlit.dataframe(My_fruit_list)
